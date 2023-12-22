c8b7e8d17cbc9565e32f3f5a53995375b6d30cb4
alamin <alaminakash9754@gmail.com>
2023-12-22 12:04:35 +0600

    added task form and connect with database

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -10,7 +10,7 @@ const Dashboard = () => {
             <h1 className="my-5 text-3xl  text-center font-medium text-yellow-500 shadow-md shadow-yellow-300">Task Management System</h1>
             <div className="flex justify-evenly shadow-lg shadow-yellow-300 items-center mt-10">
                 <h2 className="text-yellow-500 font-semibold text-xl">Hello, {user.displayName} </h2> 
-                <Link to='/createTask'><p className="text-yellow-500 text-lg font-bold">Create A Task</p></Link>
+                <Link to='/createTask'><button className="text-yellow-500 text-lg hover:bg-blue-300 p-2 rounded-lg font-bold">Create A Task</button></Link>
                 <img className="rounded-full" src={user.photoURL} alt="" />
             </div>
         </div>
